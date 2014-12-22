a8346b5a7a455596fce8a6c9d3e780e709491ef4
Daniel_Carreto <dany7carreto@gmail.com>
2014-12-22 01:12:22 -0600

    se agrego el RecyclerView con los CardView

---
+++ app/src/main/java/com/example/edwinhernandez/mdexample/ContactInfo.java
@@ -0,0 +1,14 @@
+package com.example.edwinhernandez.mdexample;
+
+/**
+ * Created by Daniel on 20/12/2014.
+ */
+public class ContactInfo {
+    protected String name;
+    protected String surname;
+    protected String email;
+    protected String titulo;
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String SURNAME_PREFIX = "Surname_";
+    protected static final String EMAIL_PREFIX = "email_";
+}
--- app/src/main/java/com/example/edwinhernandez/mdexample/FragmentDos.java
+++ app/src/main/java/com/example/edwinhernandez/mdexample/FragmentDos.java
@@ -2,17 +2,54 @@ package com.example.edwinhernandez.mdexample;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by edwinhernandez on 19/12/14.
  */
 public class FragmentDos extends Fragment {
-
+    List<ContactInfo> lista;
+    ContactInfo uno,dos;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View v = inflater.inflate(R.layout.nuevoprueba, container, false);
+
+        View v = inflater.inflate(R.layout.activity_fragmento3, container, false);
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        uno= new ContactInfo();
+        uno.name="Edwin";
+        uno.email="correo@dominiodeedwin.com";
+        uno.surname="breve descrpcion";
+        uno.titulo="Titulo Edwin";
+
+        dos= new ContactInfo();
+        dos.name="Daniel";
+        dos.email="correo@dominiodedaniel.com";
+        dos.surname="no se que sea esto";
+        dos.titulo="Titulo Daniel";
+
+        lista= new ArrayList<>();
+        lista.add(uno);
+        lista.add(dos);
+
+        RecyclerView recList = (RecyclerView)getActivity().findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity().getBaseContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+        ContactAdapter mAdapter = new ContactAdapter(lista);
+        recList.setAdapter(mAdapter);
+
     }
 }
